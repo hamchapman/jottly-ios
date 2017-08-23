6afbafe9c0e2166dd1eefb702b25d8c4b4474ec0
Hamilton Chapman <hamchapman@gmail.com>
2017-08-23 10:02:52 +0500

    Initial Commit

---
+++ jottly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jottly
+//
+//  Created by Hamilton Chapman on 23/08/2017.
+//  Copyright © 2017 hc.gg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
